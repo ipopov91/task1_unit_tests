920e5f6ac58c665acb2e3fc22b81f9d0e5f28da1
ipopov91 <mepopov91@gmail.com>
2022-03-08 20:09:02 +0300

    commit 8 march

--- src/test/java/TestPen.java
+++ src/test/java/TestPen.java
@@ -1,80 +1,132 @@
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 import static org.testng.Assert.*;
 
 public class TestPen {
 
-    @Test(dataProvider = "validationData")
-    public void check (int inkContainerValue, double sizeLetter) {
-        assertEquals(new Pen(inkContainerValue, sizeLetter).write("world is beautiful"),"world is beautiful");
-    }
-
-    @Test(dataProvider = "positiveValidationDataForWorkChecker")
-    public void workingChecker(int inkContainerValue) {
-        assertTrue(new Pen(inkContainerValue).isWork(), "true");
-    }
-
-    @Test(dataProvider = "negativeValidationDataForWorkChecker")
-    public void unworkingChecker(int inkContainerValue) {
-        assertFalse(new Pen(inkContainerValue).isWork(), "false");
-    }
-
-    @Test(dataProvider = "dataProviderForColorChecker")
-    public void colorChecker(int inkContainerValue, double sizeLetter, String color) {
-        assertEquals(new Pen(inkContainerValue, sizeLetter, color).getColor(), color);
-    }
-
     private ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-    @Test
-    public void testDoSomethingElse() {
-        PrintStream old = System.out;
-        Pen pen = new Pen(100, 1.0, "GREY");
-        System.setOut(new PrintStream(output));
+    @Test(dataProvider = "negativeValidationData")
+    public void emptyStringReturnTest (int inkContainerIsNegative) {
+        Assert.assertEquals(new Pen(inkContainerIsNegative).write("inheritance"),"");
+    }
+
+    @Test(dataProvider = "textMoreInkContainer")
+    public void testIfTextSizeMoreInkContainer(int inkContainerValue, double sizeLetter, String text, String expectedResult) {
+        Assert.assertEquals(new Pen(inkContainerValue, sizeLetter).write(text), expectedResult);
+    }
+
+    @Test(dataProvider = "textLessInkContainer")
+    public void testIfTextSizeLessInkContainer(int inkContainerValue, double sizeLetter, String text) {
+        Assert.assertEquals(new Pen(inkContainerValue, sizeLetter).write(text), text);
+    }
+
+    @Test(dataProvider = "positiveValidationData")
+    public void testIsWorkForTrue(int inkContainerValue) {
+        Pen pen = new Pen(inkContainerValue);
+        Boolean actualResult = pen.isWork();
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test(dataProvider = "negativeValidationData")
+    public void testIsWorkForFalse(int inkContainerValue) {
+        Pen pen = new Pen(inkContainerValue);
+        Boolean actualResult = pen.isWork();
+        Assert.assertFalse(actualResult);
+    }
+
+    @Test(dataProvider = "colorsData")
+    public void testGetColorMethod(String color) {
+        Pen pen = new Pen (1,1.0, color);
+        String actualResult = pen.getColor();
+        Assert.assertEquals(actualResult, color);
+    }
+
+    @Test(dataProvider = "sizeLetterParamZero")
+    public void testSizeLetterParamZero(int inkContainerValue, double sizeLetter) {
+        Pen pen = new Pen(inkContainerValue, sizeLetter);
+        String actualResult = pen.write("inheritance");
+        Assert.assertEquals(actualResult, "");
+    }
+
+    @Test(dataProvider = "positiveValidationData")
+    public void testWordParamEmptyString(int inkContainerValue) {
+        Pen pen = new Pen(inkContainerValue);
+        String actualResult = pen.write("");
+        assertEquals(actualResult, "");
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testParamNull() {
+        Pen pen = new Pen(1);
+        pen.write(null);
+    }
+
+    @Test(dataProvider = "colorsData")
+    public void testDoSthElse(String color) {
+        Pen pen = new Pen(1, 1, color);
         pen.doSomethingElse();
-        assertEquals(output.toString().replaceAll("\\s", ""), "GREY", "Successfully brings text");
-        System.setOut(old);
+        Assert.assertEquals(output.toString(), color);
     }
 
     @DataProvider
-    public static Object[][] dataProviderForColorChecker() {
+    public static Object[][] positiveValidationData() {
         return new Object[][] {
-                {1000, 1.0, "BLUE"},
-                {1000, 1.0, "RED"},
-                {1000, 1.0, "BLACK"},
-                {1000, 1.0, "GREEN"},
-        };
-    }
-
-    @DataProvider
-    public static Object[][] validationData() {
-        return new Object[][] {
-                {100, 1.0},
-                {100, 2.0},
-                {16, 1.0},
-                {16, 2.0},
-                {0, 1.0}
-        };
-    }
-
-    @DataProvider
-    public static Object[][] positiveValidationDataForWorkChecker() {
-        return new Object[][] {
+                {1},
+                {10},
                 {Integer.MAX_VALUE},
-                {100},
-                {0}
+                {Integer.MAX_VALUE - 1}
         };
     }
 
     @DataProvider
-    public static Object[][] negativeValidationDataForWorkChecker() {
+    public static Object[][] negativeValidationData() {
         return new Object[][] {
-                {-Integer.MAX_VALUE},
-                {-100},
+                {-1},
+                {-10},
+                {Integer.MIN_VALUE},
+                {Integer.MIN_VALUE + 1}
+        };
+    }
+
+
+    @DataProvider
+    public static Object[][] textMoreInkContainer() {
+        return new Object[][] {
+                {1, 1, "inheritance", "i"},
+                {5, 1, "inheritance", "inher"},
+                {10, 1, "in heritance", "in heritanc"},
+                {11, 1, "Inheritance", "Inheritanc"}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] textLessInkContainer() {
+        return new Object[][] {
+                {2, 1, "i"},
+                {23, 2, "inheritance"},
+                {Integer.MAX_VALUE, 1000, "in heritance"}
+        };
+    }
+
+    @DataProvider
+    public static Object[][] sizeLetterParamZero() {
+        return new Object[][] {
+                {1, 0}
+        };
+    }
+
+    @DataProvider
+    public Object[][] colorsData() {
+        return new Object[][]{
+                {"BLUE"},
+                {"Green"},
+                {"white"},
+                {""}
         };
     }
 }
